e51c75ff7029b39cf2f290fdfa10a07dc268b39c
yahya <yahyaest1000@gmail.com>
2023-11-10 05:10:32 +0100

    Update Navbar Notification Section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -93,24 +93,52 @@
 			</div>
 		</div>
 
-		<button class="btn btn-ghost btn-circle">
-			<div class="indicator">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-					/></svg
-				>
-				<span class="badge badge-sm indicator-item">8</span>
-			</div>
-		</button>
+		<div class="flex-none">
+			<ul class="dropdown dropdown-end">
+				<li>
+					<label class="btn btn-ghost btn-circle">
+						<button>
+							<div class="indicator">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-5 w-5"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									><path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+									/></svg
+								>
+								<span class="badge badge-sm indicator-item">8</span>
+							</div>
+						</button>
+					</label>
+					<ul
+						class="mt-3 ml-10 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-96"
+					>
+					<!-- Notifications icons with unread notif number and notifs dropdown (show last 5 notifs with unread priority , unread one have text font bold / on hover patch to seen to true) -->
+						<li>
+							<div class="flex flex-row justify-between">
+								<img
+									class="btn btn-ghost btn-circle avatar"
+									src={`${gatewayBaseUrl}/${userImage}`}
+									alt={userImage}
+								/>
+								<div>
+									<h3>Notif Username - Notif Title</h3>
+									<p>Notif Message</p>
+								</div>
+								<span>2w</span>
+							</div>
+						</li>
+						<li class="btn btn-ghost"><a href="/">See all</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 
 		{#if data.user}
 			<div class="flex-none">
