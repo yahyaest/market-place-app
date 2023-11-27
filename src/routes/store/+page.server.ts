09b0b4b3457a28a9f7d9d870dd67e4eb008093c6
yahya <yahyaest1000@gmail.com>
2023-11-27 23:04:30 +0100

    Implement Store Page

---
+++ src/routes/store/+page.server.ts
@@ -0,0 +1,39 @@
+import { redirect } from '@sveltejs/kit';
+import prisma from '$lib/prisma';
+import type { PageServerLoad } from './$types';
+
+const getUserStoreProducts = async (user: any) => {
+	if (user) {
+		const userStoreProducts = [];
+        const userProducts = await prisma.product.findMany({
+            where: { username: user.email  }
+        });
+		for (const product of userProducts) {
+			const productImages = await prisma.image.findMany({
+				where: { productId: product.id }
+			});
+
+			const userStoreProduct: any = product;
+			userStoreProduct.image = productImages[0].url;
+			userStoreProducts.push(userStoreProduct);
+		}
+		return userStoreProducts;
+	} else return null;
+};
+
+export const load = (async ({ cookies }) => {
+	if (!cookies.get('user')) {
+		throw redirect(307, `/auth/login`);
+	}
+
+	try {
+        const user = JSON.parse(cookies.get('user') as string);
+        const userStoreProducts = getUserStoreProducts(user)
+		return {
+			userStoreProducts
+		};
+	} catch (error: any) {
+		console.error('Error:', error);
+		return { error: error.message };
+	}
+}) satisfies PageServerLoad;
---
+++ src/routes/store/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	const userStoreProducts = data.userStoreProducts;
+</script>
+
+<div class="overflow-x-auto mx-auto w-4/5 sm:w-5/6">
+    <h1 class="text-center">Store</h1>
+	{#if userStoreProducts && userStoreProducts.length > 0}
+		<div class="flex flex-wrap justify-evenly">
+			{#each userStoreProducts as product}
+				<div class="card card-side bg-red-500 shadow-xl cursor-pointer mx-2 my-5 hover:-translate-y-6 transition duration-150 ease-in-out" style="width: 32rem; height:15rem;" >
+                    <div class="flex flex-auto">
+						<figure>
+							<img
+                                style="width: 15rem ; height:15rem"
+								src={product.image}
+								alt={product.title}
+							/>
+						</figure>
+						<div class="card-body">
+							<h2 class="card-title text-xs font-bold">{product.title}</h2>
+							<p>{product.price} TND</p>
+                            <div class="badge badge-secondary">6 Offers</div>
+							<div class="card-actions justify-end">
+                                <button class="btn btn-primary btn-sm">Manage</button>
+							</div>
+						</div>
+
+                    </div>
+					</div>
+			{/each}
+		</div>
+	{/if}
+</div>
