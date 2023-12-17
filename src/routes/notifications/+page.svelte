e4bd6677c31130aea2f7273c518f8f46a44f4615
yahya <yahyaest1000@gmail.com>
2023-12-17 12:56:09 +0100

    Hanle no user notifications innotifications page

--- src/routes/notifications/+page.svelte
+++ src/routes/notifications/+page.svelte
@@ -15,11 +15,11 @@
 	const user: Writable<User | null> = writable(data.user);
 	const userNotifications: Notification[] = data.userNotifications;
 
-	const pageSize: Writable<number> = writable(5);
-	const pageNumber = Math.floor(userNotifications.length / $pageSize) + 1;
+	const pageSize: Writable<number> = writable(10);
+	const pageNumber = userNotifications ? Math.floor(userNotifications.length / $pageSize) + 1 : 1;
 	const currentPage: Writable<number> = writable(1);
 	const pageNotifications: Writable<Notification[]> = writable(
-		userNotifications.slice(0, $pageSize)
+		userNotifications ? userNotifications.slice(0, $pageSize) : []
 	);
 
 	const handleNotificationUpdate = async (notificationId: number) => {
@@ -39,70 +39,71 @@
 		navbarLatestUserNotifications.set(navbarNotifications);
 		navbarNotificationsCount.update((value) => value - 1);
 	};
-
-	console.log(userNotifications[0], $pageNotifications.length);
 </script>
 
 <div class="overflow-x-auto mx-auto w-full md:w-4/5">
-	<table class="table">
-		<!-- head -->
-
-		<tbody>
-			<!-- row 1 -->
-			{#each $pageNotifications as notification, index (notification)}
-				<tr class={`hover:bg-slate-300 transition duration-150 ease-in-out`}>
-					<td>
-						<div class="flex items-center gap-3 xl:w-56">
-							<div class="avatar">
-								<div class="mask mask-squircle w-12 h-12">
-									<img
-										src={`${data.gatewayBaseUrl}/${notification.userImage}`}
-										alt={notification.username}
-									/>
+	{#if userNotifications}
+		<table class="table">
+			<tbody>
+				{#each $pageNotifications as notification, index (notification)}
+					<tr class={`hover:bg-slate-300 transition duration-150 ease-in-out`}>
+						<td>
+							<div class="flex items-center gap-3 xl:w-56">
+								<div class="avatar">
+									<div class="mask mask-squircle w-12 h-12">
+										<img
+											src={`${data.gatewayBaseUrl}/${notification.userImage}`}
+											alt={notification.username}
+										/>
+									</div>
+								</div>
+								<div>
+									<div class="font-bold">{notification.title}</div>
+									<div class="text-sm opacity-50">United States</div>
 								</div>
 							</div>
-							<div>
-								<div class="font-bold">{notification.title}</div>
-								<div class="text-sm opacity-50">United States</div>
-							</div>
-						</div>
-					</td>
-					<td>
-						{notification.message}
-						<br />
-						<span class="badge badge-primary badge-sm my-1"
-							>{formatRelativeTime(notification.createdAt)}</span
-						>
-					</td>
-					{#if !notification.seen}
+						</td>
 						<td>
-							<button
-								class="btn btn-secondary btn-xs"
-								on:click={() => handleNotificationUpdate(notification.id)}>Mark As Read</button
+							{notification.message}
+							<br />
+							<span class="badge badge-primary badge-sm my-1"
+								>{formatRelativeTime(notification.createdAt)}</span
 							>
 						</td>
-					{:else}
-						<td />
-					{/if}
-				</tr>
+						{#if !notification.seen}
+							<td>
+								<button
+									class="btn btn-secondary btn-xs"
+									on:click={() => handleNotificationUpdate(notification.id)}>Mark As Read</button
+								>
+							</td>
+						{:else}
+							<td />
+						{/if}
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		<div class="join my-10 flex justify-center">
+			{#each Array.from({ length: pageNumber }, (_, i) => i + 1) as pageIndex}
+				<button
+					class={`join-item btn ${
+						$currentPage === pageIndex
+							? 'btn-primary'
+							: 'hover:bg-slate-300 transition duration-150 ease-in-out'
+					}`}
+					on:click={() => {
+						currentPage.set(pageIndex);
+						pageNotifications.set(
+							userNotifications.slice((pageIndex - 1) * $pageSize, pageIndex * $pageSize)
+						);
+					}}>{pageIndex}</button
+				>
 			{/each}
-		</tbody>
-	</table>
-	<div class="join my-10 flex justify-center">
-		{#each Array.from({ length: pageNumber }, (_, i) => i + 1) as pageIndex}
-			<button
-				class={`join-item btn ${
-					$currentPage === pageIndex
-						? 'btn-primary'
-						: 'hover:bg-slate-300 transition duration-150 ease-in-out'
-				}`}
-				on:click={() => {
-					currentPage.set(pageIndex);
-					pageNotifications.set(
-						userNotifications.slice((pageIndex - 1) * $pageSize, pageIndex * $pageSize)
-					);
-				}}>{pageIndex}</button
-			>
-		{/each}
-	</div>
+		</div>
+	{:else}
+		<div>
+			<h1 class="text-center text-2xl font-extrabold">No Notifications were found</h1>
+		</div>
+	{/if}
 </div>
